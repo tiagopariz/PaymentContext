a628df24b8c309a52bf4e54b19e607b9b372e264
tiagopariz <tiagopariz@gmail.com>
2018-06-02 18:43:46 -0300

    Aula 8: Corrupção no Código

--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -1,12 +1,39 @@
+using System;
+using System.Collections.Generic;
+
 namespace PaymentContext.Domain.Entities
 {
     public class Student
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Document { get; set; }
-        public string Email { get; set; }
-        public string Address { get; set; }
-        public List<Subscription> Subscriptions { get; set; }
+        public ClassName(string firstName,
+                         string lastName,
+                         string Document,
+                         string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Document = Document;
+            Email = email;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Document { get; private set; }
+        public string Email { get; private set; }
+        public string Address { get; private set; }
+        public List<Subscription> Subscriptions { get; private set; }
+
+        public void AddSubscription(Subscription subscription)
+        {
+            // Se já tiver uma assinatura, cancela
+
+            // Cancela todas as outras assinaturas e coloca esta
+            // como principal
+            foreach (var sub in Subscriptions)
+            {
+                sub.Active = false;
+                Subscriptions.Add(sub);
+            }
+        }
     }
 }
---
+++ PaymentContext.Tests/Entitites/StudentTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var student = new Student("André", "Baltieri", "12345678912", "hello@balta.io");
+        }
+    }
+}
