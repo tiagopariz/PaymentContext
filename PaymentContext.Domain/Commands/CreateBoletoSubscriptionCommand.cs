7ac078890b3a841967d93ebda44c4f00c36599f4
tiagopariz <tiagopariz@gmail.com>
2018-06-12 13:49:30 -0300

    Aula 15: Commands

---
+++ PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
@@ -0,0 +1,31 @@
+using System;
+using PaymentContext.Domain.ValueObjects;
+using PaymentContext.Domain.Enums;
+
+namespace PaymentContext.Domain.Commands
+{
+    public class CreateBoletoSubscriptionCommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public string Email { get; set; }
+        public string Barcode { get; set; }
+        public string BoletoNumber { get; set; }
+        public string PaymentNumber { get; set; }
+        public DateTime PaidDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public decimal Total { get; set; }
+        public decimal TotalPaid { get; set; }
+        public string Payer { get; set; }
+        public Document PayerDocument { get; set; }
+        public EDocumentType PayerDocumentType { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string Neighborhood { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
---
+++ PaymentContext.Domain/Commands/CreateCreditCardSubscriptionCommand.cs
@@ -0,0 +1,32 @@
+using System;
+using PaymentContext.Domain.ValueObjects;
+using PaymentContext.Domain.Enums;
+
+namespace PaymentContext.Domain.Commands
+{
+    public class CreateCreditCardSubscriptionCommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public string Email { get; set; }
+        public string Barcode { get; set; }
+        public string CardHolderName { get; set; }
+        public string CardNumber { get; set; }
+        public string LastTransactionName { get; set; }
+        public DateTime PaidDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public decimal Total { get; set; }
+        public decimal TotalPaid { get; set; }
+        public string Payer { get; set; }
+        public Document PayerDocument { get; set; }
+        public EDocumentType PayerDocumentType { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string Neighborhood { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
